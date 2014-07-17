e7bdd5dd7de85ad09738342a304a61b072ec0d2d
go123 <cz2051@163.com>
2014-07-17 18:15:42 +0800

    sds

--- protected/controllers/BusinessController.php
+++ protected/controllers/BusinessController.php
@@ -26,7 +26,7 @@ class BusinessController extends CController{
 			$nums = Authorizer::model()->countByAttributes(array('uid'=>Yii::app()->user->id));
 			$userinfo = User2::model()->findByAttributes(array('username'=>Yii::app()->user->name),array('select'=>'groupid'));
 			$usergroup = Group::model()->findByPk($userinfo->groupid);
-			if($nums > $usergroup->version_nums){
+			if($nums == $usergroup->version_nums){
 				CV::showmsg('域名授权已经达到上限，请联系管理员',Yii::app()->createUrl('business/add'));
 			}
 			$model = new Authorizer();
