19c00b50f9d80e502e07fdf4795e64ae4168567b
go123 <cz2051@163.com>
2014-07-20 17:34:48 +0800

    ss

--- protected/controllers/YunyueController.php
+++ protected/controllers/YunyueController.php
@@ -1,8 +1,18 @@
 <?php
 class YunyueController extends CController{
 	public function actionLog(){
+		$datalog = Version::model()->findAllByAttributes(array(),array('order'=>'posttime desc'));
+		$i=0;
+		foreach ($datalog as $v){
+			$i++;
+			if($i%2==0){
+				$datalog1[] = $v;
+			}else{
+				$datalog2[] = $v;
+			}
+		}
 		$this->pageTitle = '更新日志'.'--'.Yii::app()->params['sitename'];
-		$this->render('log');
+		$this->render('log',array('datalog1'=>$datalog1,'datalog2'=>$datalog2));
 	}
 	public function actionVs(){
 		$this->render('vs');
