16a1553361537227422554d19928caf86872d4e9
go123 <cz2051@163.com>
2014-07-20 18:30:58 +0800

    ss

--- protected/views/business/index.php
+++ protected/views/business/index.php
@@ -28,11 +28,11 @@
 	<tr style="height:30px;line-height: 30px;border-bottom:1px #ccc solid;">
 	<th style="width:6%">序号</th>
 	<th style="width:20%">域名</th>
-	<th style="width:18%">时间</th>
+	<th style="width:15%">时间</th>
 	<th style="width:9%">类型</th>
 	<th style="width:9%">版本</th>
 	<th style="width:30%">授权码</th>
-	<th style="width:6%">重新授权</th>
+	<th style="width:9%">操作</th>
 	</tr>
 	<?php $i=1; foreach($your as $v):?>
 	<tr style="border-bottom:1px dashed #ccc;">
